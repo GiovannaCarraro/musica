8a065f043dda8ffe9b025e9d599b8871a55dfbe1
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-02-12 17:22:38 -0300

    fazendo o projetinho

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+import mysql.connector
 
 app = Flask (__name__)
+
+@app.route("/home", methods=["GET"])
+@app.route("/")
+def pagina_principal():
+
+    #Conectando no bd
+    conexao = mysql.connector.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        password="root",
+        database="Musica"
+    )
+
+    #Criando o cursor
+    cursor = conexao.cursor(dictionary=True)
+
+    #Executando a consultando
+    cursor.execute("SELECT codigo, cantor, duracao, nome, url_imagem, nome_genero FROM musica;")
+
+    #rec os dados
+    musicas =  cursor.fetchall()
+
+    #fechando a conexao
+    conexao.close()
+
+
+    return render_template("principal.html", musicas = musicas)
 
 if __name__ == "__main__":
     app.run(debug=True)
