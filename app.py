90db63b2a9589462b22888b779eecca7728a4c93
GiovannaCarraro <giovanna.c.oliveia9@aluno.senai.br>
2026-02-20 17:14:43 -0300

    finalizando o insert

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import mysql.connector
 from model.musica import recuperar_musicas
 from model.genero import recuperar_generos
+from model.musica import adicionar_musica
 
 app = Flask (__name__)
 
@@ -27,5 +28,17 @@ def pagina_admin():
     #mostra a pag
     return render_template("administracao.html", musicas = musicas, generos = generos)
 
+@app.route("/musica/post", methods=["POST"])
+def api_inserir_musica():
+    nome_musica = request.form.get("nome_musica")
+    cantor = request.form.get("cantor")
+    duracao = request.form.get("duracao")
+    imagem  = request.form.get("imagem")
+    genero = request.form.get("genero")
+    if  adicionar_musica(nome_musica, cantor, duracao, imagem, genero):
+        return redirect("/admin")
+    else:
+        return "erro ao adicionar musica"
+    
 if __name__ == "__main__":
     app.run(debug=True)
