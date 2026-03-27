56670604ff52d8ad966a9d3a0e711416fd3e4a5d
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-03-27 15:44:11 -0300

    conexao

--- database/conexao.py
+++ database/conexao.py
@@ -3,6 +3,8 @@ import mysql.connector
   
 def conectar():
         #Conectando no bd
+    tipo_conexao = "Nuvem"
+    if tipo_conexao == "Local":
         conexao = mysql.connector.connect(
             host="localhost",
             port=3306,
@@ -12,6 +14,14 @@ def conectar():
             )
 
             #Criando o cursor
+    else:
+        conexao = mysql.connector.connect(
+            host="servidor-giovanna-servidor-giovanna.a.aivencloud.com",
+            port=28596,
+            user="avnadmin",
+            password="AVNS_7GpD5nPMfNurZLaW2xD",
+            database="Musica")
+
         cursor = conexao.cursor(dictionary=True)
 
         return conexao, cursor
